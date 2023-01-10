2f22a8d2aadb60b70032f9478d4dd41ab2a6283b
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-01-10 17:40:10 -0600

    added top movies and app.get

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 const express = require('express');
-const app = express;
+const app = express();
+
+let topMovies = [
+    {
+        title: 'Dangerous Minds',
+        year: 1995
+    },
+    {
+        title: 'Die Hard',
+        year: 1988
+    },
+    {
+        title: 'Top Gun: Maverick',
+        year: 2022
+    },
+    {
+        title: 'The Lion King',
+        year: 1994
+    },
+    {
+        title: 'Gone with the Wind',
+        year: 1939
+    },
+    {
+        title: 'A Few Good Men',
+        year: 1992
+    },
+    {
+        title: 'The Wizard of Oz',
+        year: 1939
+    },
+    {
+        title: 'The Equalizer',
+        year: 2014
+    },
+    {
+        title: 'Contact',
+        year: 1997
+    },
+    {
+        title: 'The Horse Whisperer',
+        year: 1998
+    }
+]
+
+//GET requests
+app.get('/',(req, res) => {
+    res.send('Welcome to myFlix');
+});
+
+app.get('/movies',(req, res) => {
+    res.json(topMovies);
+});
+
+//listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080');
+});
