e46ac8e1f79c8eed10b9b884f0ccf261293a09a3
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-04-30 10:14:35 -0500

    added app url from netlify to CORS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix-ptn.netlify.app'];
 
 app.use(cors({
     origin: (origin, callback) => {
