808b290c98a228394cb4ec7df35dcabedb51f04e
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2023-07-17 18:13:40 -0500

    added CORS origin for AWS instance

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:4200', 'http://testsite.com', 
-                        'http://localhost:1234', 'https://myflix-ptn.netlify.app', 'https://ptn6389.github.io'];
+                        'http://localhost:1234', 'https://myflix-ptn.netlify.app', 'https://ptn6389.github.io',
+                        'http://18.218.255.210', 'https://18.218.255.210'];
 
 app.use(cors({
     origin: (origin, callback) => {
